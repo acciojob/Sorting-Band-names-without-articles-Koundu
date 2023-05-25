08a361b985db3675663dc97bd53040be270bccca
Koundu <121346044+Koundu@users.noreply.github.com>
2023-05-25 14:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple','Victoria Memorial','Tajmahal'];
 
+const orderedSpots = touristSpots.sort((a,b) =>{
+	const regex = /^(?:The|A|An)\s+/i;
+
+	const spotA = a.replace(regex, "");
+	const spotB = b.replace(regex, "");
+
+	return spotA.localCompare(spotB);
+});
+
+console.log(orderedSpots)
