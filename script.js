537f79c403c0bf32a58c11e6af1b4d77a650fcc9
Koundu <121346044+Koundu@users.noreply.github.com>
2023-06-07 15:36:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ function strip(word){
 	return word.replace(regex, '').trim()
 }
 const sortedBands = touristsSopts.sort((a,b) => (strip(a)>strip(b)) ? 1 : -1)
-document.querySelector('#bands').innerHTML = sortedBands.map(item => `<li>${item}</li>`).join('')
+document.querySelector('#band').innerHTML = sortedBands.map(item => `<li>${item}</li>`).join('')
