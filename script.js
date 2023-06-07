c3e4551141ef7402367703a480e16ec3786cbe9b
Koundu <121346044+Koundu@users.noreply.github.com>
2023-06-07 15:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-//your code here
-let touristsSopts = ['The Virupaksha Temple', 'Vicoria Memorial', 'Tajmahal'];
-function strip(word){
-	let regex = new RegExp('^(a | an | the'),i)
-	return word.replace(regex, '').trim()
+const band = [
+    "The Plot in You",
+    "The Devil Wears Prada",
+    "Pierce the Veil",
+    "Norma Jean",
+    "The Bled",
+    "Say Anything",
+    "The Midway State",
+    "We Came as Romans",
+    "Counterparts",
+    "Oh, Sleeper",
+    "A Skylit Drive",
+    "Anywhere But Here",
+    "An Old Dog"
+];
+
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
 }
-const sortedBands = touristsSopts.sort((a,b) => (strip(a)>strip(b)) ? 1 : -1)
-document.querySelector('#band').innerHTML = sortedBands.map(item => `<li>${item}</li>`).join('')
+
+const sortedBands = band.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+
+document.querySelector('#band').innerHTML = 
+    sortedBands.map(item => `<li>${item}</li>`).join('')
